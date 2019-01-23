b8a59250a3f2a0d49a6ba66f1523cc3ed03cb038
RommiPlus <>
2019-01-23 10:45:36 +0800

    增加获取网络请求并解析字符串显示在界面上内容

--- http_get/lib/main.dart
+++ http_get/lib/main.dart
@@ -48,18 +48,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                       list[index].name,
                                       style: TextStyle(color: Colors.blue, fontSize: 24),
                                     ),
-                                  )
+                                  ),
                                 ],
                               ),
                               margin: const EdgeInsets.only(left: 4, top: 2, right: 4, bottom: 3),
