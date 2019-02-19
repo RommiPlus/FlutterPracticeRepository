7de8d99d67b0d64d62bf5295b6e037e225ce6d04
RommiPlus <>
2019-02-19 17:38:20 +0800

    调用原生平台的代码demo

--- write_specific_platform_demo/android/app/src/main/java/com/rommiplus/writespecificplatformdemo/MainActivity.java
+++ write_specific_platform_demo/android/app/src/main/java/com/rommiplus/writespecificplatformdemo/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
@@ -25,6 +29,7 @@ public class MainActivity extends FlutterActivity {
                 new MethodChannel.MethodCallHandler() {
                     @Override
                     public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                        // 使用这种方式也可以动态添加view
                         if (methodCall.method.equals("getBatteryLevel")) {
                             int batteryLevel = getBatteryLevel();
 
@@ -54,5 +59,16 @@ public class MainActivity extends FlutterActivity {
         }
 
         return batteryLevel;
+    }
+
+    private void addView2Layout() {
+        //add到现有的view
+        FrameLayout v = (FrameLayout) findViewById(android.R.id.content);
+        View linearLayout = new LinearLayout(MainActivity.this);
+        linearLayout.setBackgroundColor(0xff0000ff);
+        FrameLayout.LayoutParams marginLayoutParams = new FrameLayout.LayoutParams(400, 400);
+        marginLayoutParams.setMargins(400, 400, 0, 0);
+        linearLayout.setLayoutParams(marginLayoutParams);
+        v.addView(linearLayout);
     }
 }
