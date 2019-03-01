166df73487c5624eaca61601fd76c8a834e494c6
RommiPlus <>
2019-03-01 17:29:12 +0800

    增加页面跳转时数据传输demo

---
+++ navigator_with_param/lib/second_page.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SecondPage extends StatefulWidget {
+  final String name;
+
+
+  SecondPage(this.name);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SecondPageState();
+  }
+
+}
+
+class SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('second page title'),
+      ),
+      body: Text(widget.name ?? 'no data'),
+      floatingActionButton: FloatingActionButton(onPressed: callback),
+    );
+  }
+
+
+  void callback() {
+    Navigator.pop(context, widget.name);
+  }
+}
