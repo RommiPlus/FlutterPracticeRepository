3a862437aec7a93ee7b0678feb8b2686a335dc9a
RommiPlus <>
2019-05-21 14:10:35 +0800

    feat：增加android后台时收到通知页面指定跳转功能

--- jpushDemo/jpush_demo/android/app/src/main/java/com/omron/jpush_demo/MainActivity.java
+++ jpushDemo/jpush_demo/android/app/src/main/java/com/omron/jpush_demo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.omron.jpush_demo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import java.util.IllegalFormatCodePointException;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    MethodChannel methodChannel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        methodChannel = new MethodChannel(getFlutterView(), "pageControl");
+        methodChannel.setMethodCallHandler((methodCall, result) -> {
+            if (methodCall.method.equals("toBackGround")) {
+                moveTaskToBack(true);
+            }
+        });
+    }
+
 }
