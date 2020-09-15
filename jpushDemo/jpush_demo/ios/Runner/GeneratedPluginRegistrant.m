bb377cc0495a359be522d4baf74877f9040ebd04
唐俊青 <junqingt@gmail.com>
2020-09-15 14:15:39 +0800

    feat: 追加相关demo

---
+++ jpushDemo/jpush_demo/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <jpush_flutter/JPushPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [JPushPlugin registerWithRegistrar:[registry registrarForPlugin:@"JPushPlugin"]];
+}
+
+@end
